5a9d515c86c7122bf013809a9aeddb2d37cac1f0
DANIZIN <98532797+Danibomfin@users.noreply.github.com>
2022-08-16 06:43:57 -0300

    Add files via upload

--- source/meta/data/dependency/Discord.hx
+++ source/meta/data/dependency/Discord.hx
@@ -1,8 +1,5 @@
 package meta.data.dependency;
 
-#if !html5
-import discord_rpc.DiscordRpc;
-#end
 import lime.app.Application;
 
 /**
@@ -15,13 +12,6 @@ class Discord
 	// set up the rich presence initially
 	public static function initializeRPC()
 	{
-		DiscordRpc.start({
-			clientID: "962858524826800158",
-			onReady: onReady,
-			onError: onError,
-			onDisconnected: onDisconnected
-		});
-
 		// THANK YOU GEDE
 		Application.current.window.onClose.add(shutdownRPC);
 	}
@@ -29,12 +19,12 @@ class Discord
 	// from the base game
 	static function onReady()
 	{
-		DiscordRpc.presence({
+		/*DiscordRpc.presence({
 			details: "",
 			state: null,
-			largeImageKey: 'freaky',
-			largeImageText: "FNF in: Funkin for Bikini Bottom!"
-		});
+			largeImageKey: 'iconog',
+			largeImageText: "Forever Engine"
+		});*/
 	}
 
 	static function onError(_code:Int, _message:String)
@@ -49,26 +39,12 @@ class Discord
 
 	//
 
-	/* This function contains a new installation at the end called 'icon' which will track the discord image being used for the certain states you're in the game.
-		Due to 'largeImageKey' reading a String for a picture, the 'icon' variable that's a String is used for 'largeImageKey' to track for each song and state.
-	    Me and Decoy spent a little while figuring this out and he's mainly responsible for the idea. We did get inspiration from Afton, so credits to them. - doubletime32 */
-	public static function changePresence(details:String = '', state:Null<String> = '', ?smallImageKey:String, ?hasStartTimestamp:Bool, ?endTimestamp:Float, ?icon: String)
+	public static function changePresence(details:String = '', state:Null<String> = '', ?smallImageKey:String, ?hasStartTimestamp:Bool, ?endTimestamp:Float)
 	{
 		var startTimestamp:Float = (hasStartTimestamp) ? Date.now().getTime() : 0;
 
 		if (endTimestamp > 0)
 			endTimestamp = startTimestamp + endTimestamp;
-
-		DiscordRpc.presence({
-			details: details,
-			state: state,
-			largeImageKey: icon,
-			largeImageText: "FNF in: Funkin for Bikini Bottom!",
-			smallImageKey: smallImageKey,
-			// Obtained times are in milliseconds so they are divided so Discord can use it
-			startTimestamp: Std.int(startTimestamp / 1000),
-			endTimestamp: Std.int(endTimestamp / 1000)
-		});
 
 		// trace('Discord RPC Updated. Arguments: $details, $state, $smallImageKey, $hasStartTimestamp, $endTimestamp');
 	}
@@ -76,7 +52,6 @@ class Discord
 	public static function shutdownRPC()
 	{
 		// borrowed from izzy engine -- somewhat, at least
-		DiscordRpc.shutdown();
 	}
 	#end
 }
